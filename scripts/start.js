1397c81d51055c83106a40051d506d485b2459e5
Felipe Figueiredo <lipefl@gmail.com>
2021-12-31 16:45:34 -0300

    v1.0.3
    minor update

--- scripts/start.js
+++ scripts/start.js
@@ -1,5 +1,12 @@
 Hooks.on("updateActor", (actor,changes,diff,userId) => {
+
    if (game.user.id==userId && changes.name!==undefined){
     actor.update({"token.name":changes.name});
+   
+    if (actor.isToken){
+      canvas.tokens.get(actor.token.id).data.update({"name":changes.name});
+    }
+   
+    
    }
 });
